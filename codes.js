30c94e3fc48416d01c342fa058b755618f40d0e2
Oksana <oksanapas@yahoo.com>
2024-07-29 15:59:22 +0300

    Add files via upload

---
+++ codes.js
@@ -0,0 +1,186 @@
+function content59(){
+  var x = document.getElementById("content59");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function content1012(){
+  var x = document.getElementById("content1012");
+    var x1 = document.getElementById("contentname");
+  if (x.style.display === "none") {
+    x.style.display = "block";x1.style.display = "block";
+  } else {
+    x.style.display = "none";x1.style.display = "none";
+  }  
+}
+
+function standard(){
+  var x = document.getElementById("standard");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function compot(){
+  var x = document.getElementById("compot");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function soft(){
+  var x = document.getElementById("soft");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function sopen(a){
+  var kk=['s1','s2','s3','s4','s5','s6','s7','s8','s9','s10','s11'];
+  for (let x = 0; x < kk.length; x++){
+	  var b = document.getElementById(kk[x]);
+	  b.style.display = "none";
+	  }
+  var x = document.getElementById(a);
+  if (x.style.display === "none") {
+    x.style.display = "";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function kopen(a){
+  var kk=['k1','k2','k3','k4','k5','k6','k7','k8','k9','k10','k11','k12','k13'];
+  for (let x = 0; x < kk.length; x++){
+	  var b = document.getElementById(kk[x]);
+	  b.style.display = "none";
+	  }
+  var x = document.getElementById(a);
+  if (x.style.display === "none") {
+    x.style.display = "";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function orients56(){
+  var mas = document.getElementsByClassName("orient56"); 
+  for (let x = 0; x < mas.length; x++){
+  if (mas[x].style.display === "none") {
+    mas[x].style.display = "";
+	document.getElementById("button_orients56").style.background = "#7163B4";
+  } else {
+    mas[x].style.display = "none";
+	document.getElementById("button_orients56").style.background = "#ada9bb";
+  }
+  }
+}
+
+function orients79(){
+  var mas = document.getElementsByClassName("orient79");
+  for (let x = 0; x < mas.length; x++){
+  if (mas[x].style.display === "none") {
+    mas[x].style.display = "";
+	document.getElementById("button_orients79").style.background = "#7163B4";	
+  } else {
+    mas[x].style.display = "none";
+	document.getElementById("button_orients79").style.background = "#ada9bb";
+  }
+  }
+}
+
+function orients1op(){
+  var mas = document.getElementsByClassName("orient1o");
+  for (let x = 0; x < mas.length; x++){
+  if (mas[x].style.display === "none") {
+    mas[x].style.display = "";
+	document.getElementById("button_orients1op").style.background = "#7163B4";
+  } else {
+    mas[x].style.display = "none";
+	document.getElementById("button_orients1op").style.background = "#ada9bb";
+  }
+  }
+  var mas = document.getElementsByClassName("orient1p");
+  for (let x = 0; x < mas.length; x++){
+  if (mas[x].style.display === "none") {
+    mas[x].style.display = "";
+	document.getElementById("button_orients1op").style.background = "#7163B4";
+  } else {
+    mas[x].style.display = "none";
+	document.getElementById("button_orients1op").style.background = "#ada9bb";
+  }
+  }
+}
+
+
+function kors12(){
+  var mas = document.getElementsByClassName("kor12"); 
+  for (let x = 0; x < mas.length; x++){
+  if (mas[x].style.display === "none") {
+    mas[x].style.display = "";
+	document.getElementById("button_kors12").style.background = "#AE71A5";
+  } else {
+    mas[x].style.display = "none";
+	document.getElementById("button_kors12").style.background = "#ada9bb";
+  }
+  }
+}
+
+function kors34(){
+  var mas = document.getElementsByClassName("kor34"); 
+  for (let x = 0; x < mas.length; x++){
+  if (mas[x].style.display === "none") {
+    mas[x].style.display = "";
+	document.getElementById("button_kors34").style.background = "#AE71A5";
+  } else {
+    mas[x].style.display = "none";
+	document.getElementById("button_kors34").style.background = "#ada9bb";
+  }
+  }
+}
+
+function kors56(){
+  var mas = document.getElementsByClassName("kor56"); 
+  for (let x = 0; x < mas.length; x++){
+  if (mas[x].style.display === "none") {
+    mas[x].style.display = "";
+	document.getElementById("button_kors56").style.background = "#AE71A5";
+  } else {
+    mas[x].style.display = "none";
+	document.getElementById("button_kors56").style.background = "#ada9bb";
+  }
+  }
+}
+
+function kors79(){
+  var mas = document.getElementsByClassName("kor79"); 
+  for (let x = 0; x < mas.length; x++){
+  if (mas[x].style.display === "none") {
+    mas[x].style.display = "";
+	document.getElementById("button_kors79").style.background = "#AE71A5";
+  } else {
+    mas[x].style.display = "none";
+	document.getElementById("button_kors79").style.background = "#ada9bb";
+  }
+  }
+}
+
+function kors10(){
+  var mas = document.getElementsByClassName("kor10"); 
+  for (let x = 0; x < mas.length; x++){
+  if (mas[x].style.display === "none") {
+    mas[x].style.display = "";
+	document.getElementById("button_kors10").style.background = "#AE71A5";
+  } else {
+    mas[x].style.display = "none";
+	document.getElementById("button_kors10").style.background = "#ada9bb";
+  }
+  }
+}
